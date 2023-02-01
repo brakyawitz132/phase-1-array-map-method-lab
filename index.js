e35cc3134b61e4d3a4c924b2fa6f7d7a7b1f2b24
Jacob Park <jacobypark122@gmail.com>
2023-02-01 15:55:11 -0500

    created function

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// const titleCased = () => {
+//   return tutorials
+// }
+
+// console.log(tutorials[0][2])
+
+function titleCased() {
+  const fixedTitles = tutorials.map(title => {
+    const words = title.split(" ") // split up words to access individual words
+    return words.map(word => {
+      return word[0].toUpperCase()+word.slice(1) // change the first letter to upper case and add the other letters
+    }).join(" ") // add all words together to get full string
+  })
+  return fixedTitles
 }
